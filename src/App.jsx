d7384773656b378fb424a11978289b6626e3a62a
Jimmy Jouanne <jimmy.jouanne@jobteaser.com>
2023-03-27 16:39:17 +0200

    fix(component): remove vite logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ import { BumpButton } from "./components/BumpButton/BumpButton";
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Awesome Button react</h1>
       <h2>Oh nooo what happened ?!</h2>
       <div className="card">
